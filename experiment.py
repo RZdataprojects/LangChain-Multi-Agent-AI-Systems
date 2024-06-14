44c32693637b6ed2daa7e04b8db39c59a1656147
RZdataprojects <egdv12@gmail.com>
2024-06-14 21:58:16 +0300

    finished framework, needs to decide whether to keep gemini vs gemini

--- experiment.py
+++ experiment.py
@@ -1,6 +1,6 @@
 import models
-import pandas as pd
 import re
+from langchain_core.messages import AIMessage, HumanMessage
 import torch
 import numpy as np
 np.random.seed(0)
@@ -8,28 +8,36 @@ torch.manual_seed(0)
 
 
 class Experiment():
-    def __init__(self, google_api_key: str, hugging_face_token: str, num_of_games: int = 30, num_of_rounds: int = 6):
+    def __init__(self, google_api_key: str, hugging_face_token: str, num_of_games: int = 30, num_of_rounds: int = 6, verbose: int = 1):
         """
-        
+        :param google_api_key:
+        :param hugging_face_token:
         """
         self.gemini = models.initialize_gemini(google_api_key=google_api_key)
-        self.mistral = models.initialize_mistral(hugging_face_token=hugging_face_token)
+        self.mistral = models.initialize_gemini(google_api_key=google_api_key)
         self.games = []
         self.num_of_games = num_of_games
         self.num_of_rounds = num_of_rounds
+        self.verbose = verbose
 
     def play(self, mode: int) -> list[list]:
-    """
-        
-    """
-        for game_idx in np.arange(self.num_of_games)+1:
-            history = []
-            game_result = self.one_game(mode, history)
-            self.games.append(game_result)
-        return self.games
+        """
 
-    def one_game(self, mode) -> list[dict]:
-        rounds = []
+        """
+        gemini_sum, mistral_sum = 0, 0
+        for game_idx in np.arange(self.num_of_games)+1:
+            if self.verbose:
+                print(f"running game {game_idx}")
+            results = self.one_game(mode)
+            gemini_sum += results[0]
+            mistral_sum += results[1]
+            self.games.append(results)
+        return {'gemini sum': gemini_sum,
+                'mistral sum': mistral_sum,
+                'history': self.games}
+
+    def one_game(self, mode) -> tuple[float, float, dict]:
+        history = []
         gemini_sum = 0
         mistral_sum = 0
         for round_idx in np.arange(self.num_of_rounds)+1:
@@ -39,69 +47,87 @@ class Experiment():
             else:
                 proposer = "mistral"
                 responder = "gemini"
-            round_result = self.one_round(proposer, responder, mode)
-            gemini_sum += round_result.gemini_sum
-            mistral_sum += round_result.mistral_sum
-            rounds.append(round_result)
-        return rounds
+            if self.verbose:
+                print(f"round {round_idx}", end=" ")
+            round_result = self.one_round(proposer, responder, mode, history)
+            round_result['round'] = round_idx
+            # print(round_result)
+            history.append(round_result)
+            gemini_sum += round_result['gemini_sum']
+            mistral_sum += round_result['mistral_sum']
+        return gemini_sum, mistral_sum, history
 
     def one_round(self, proposer, responder, mode, history) -> dict:
         """
         
         """
-        proposer_role = assign_roles(mode, proposer)
-        responder_role = assign_roles(mode, responder)
-        proposal, reasoning = self.get_proposal(proposer=proposer,
+        proposer_role = self.assign_roles(mode, proposer)
+        responder_role = self.assign_roles(mode, responder)
+        if self.verbose:
+            print(f"Proposer: {proposer} as {proposer_role}, Responder: {responder} as {responder_role}", end=" ")
+        proposer_sum, responder_sum, proposal_reasoning = self.get_proposal(proposer=proposer,
                                                 proposer_role=proposer_role,
                                                 responder=responder,
                                                 responder_role=responder_role,
                                                 mode=mode,
                                                 history=history)
-        response, reasoning = self.get_response(responder=responder,
+
+        response, response_reasoning = self.get_response(responder=responder,
                                                 responder_role=responder_role,
                                                 proposer=proposer,
                                                 proposer_role=proposer_role,
+                                                proposer_sum=proposer_sum,
+                                                responder_sum=responder_sum,
                                                 mode=mode,
                                                 history=history)
         if response == 'accept':
-            proposal = proposal.split('/')
-            gemini_sum = float(re.findall(r'\d+\.?\d*', proposal[0]))
-            mistral_sum = float(re.findall(r'\d+\.?\d*', proposal[1]))
+            if proposer == 'gemini':
+                gemini_sum = proposer_sum
+                mistral_sum = responder_sum
+            else:
+                gemini_sum = responder_sum
+                mistral_sum = proposer_sum
         else:
             gemini_sum = 0
             mistral_sum = 0
-        return {'proposer': proposer, 'proposer_role': proposer_role, 'proposal': proposal,
-                'responder': responder, 'responder_role': responder_role, 'response': response,
+        if ~self.verbose:
+            print(f"Proposal: {proposer_sum}|{responder_sum} - Reasoning: {proposal_reasoning}, Response: {response} - Reasoning: {response_reasoning}") 
+        return {'proposer': proposer, 'proposer_role': proposer_role, 
+                'proposal': f"{proposer_sum}|{responder_sum}", 'proposal reasoning': proposal_reasoning,
+                'responder': responder, 'responder_role': responder_role, 
+                'response': response, 'response reasoning': response_reasoning,
                 'gemini_sum': gemini_sum,
                 'mistral_sum': mistral_sum
                }
 
-    def get_proposal(*, self, proposer, proposer_role, responder, responder_role, mode, history):
+    def get_proposal(self, *, proposer, proposer_role, responder, responder_role, mode, history):
         """
         
         """
-        message = HumanMessage(
-                                content=[
+        message = HumanMessage(content=[
                                     {
                                         "type": "text",
                                         "text": f"""You are a {proposer_role} and are given insturctions by the user. 
                                         Answer the insturctions with under 10 words and ONLY in the format:
-                                            response:
+                                            proposer_sum:
+                                            responder_sum:
                                             reasoning:
-                                            YOU MAY NOT ANSWER WITHOUT USING THE FORMAT UNDER 10 WORDS."""
+                                            YOU MAY NOT ANSWER WITHOUT USING THE FORMAT AND UNDER 10 WORDS."""
                                     },  
                                     {
-                                        "type": "text", "text": """
-                                        You are playing the ultimatum game against 'gemma'.
+                                        "type": "text", "text": f"""
+                                        You are playing the ultimatum game against a {responder_role}.
                                         In stage 1 of the ultimatum game,
-                                        the first player (Proposer) proposes a specific split of a fixed amount of money of $100,
-                                        to the second player.
-                                        In stage 2 the second player (Responder) can either accept the proposed split or reject it.
+                                        the first player (Proposer) proposes a specific split of a fixed
+                                        amount of money of $100 to the second player.
+                                        In stage 2 the second player (Responder) can either accept 
+                                        the proposed split or reject it.
                                         If they accept the proposal, 
                                         the fixed amount is divided according to the first mover's proposal.
                                         If they choose to reject the proposal, the players do not receive anything."""},
                                     {
-                                        "type": "text", "text": """You are the proposer.
+                                        "type": "text", "text": """
+                                        You are the proposer.
                                         Please propose a split of $100 between yourself and the responder.
                                         You may use information from previous games if any is available."""
                                     },
@@ -112,51 +138,65 @@ class Experiment():
                             )
              
         if proposer == 'gemini':
-            proposal = self.gemini.invoke([message]).content
+            proposal = self.gemini.invoke([message])
         else:
-            proposal = self.mistral.invoke([message]).content                              
-        return proposal.split()[1], proposal.split()[-1]
+            proposal = self.mistral.invoke([message])
+        if isinstance(proposal, str):
+            proposal = proposal.lower()
+        else:
+            proposal = proposal.content.lower()
+        sums = re.findall(r'\d+\.?\d*', proposal)
+        proposal_sum, responder_sum = float(sums[0]), float(sums[1])
+        return proposal_sum, responder_sum, proposal.split('reasoning: ')[-1]
 
-    def get_response(*, self, responder, responder_role, proposer, proposer_role, mode, history):
+    def get_response(self, *, responder, responder_role, proposer, proposer_role, proposer_sum, responder_sum, mode, history):
         """
         
         """
-        message = HumanMessage(
-                                content=[
+        message = HumanMessage(content=[
                                     {
                                         "type": "text",
-                                        "text": """You are given insturctions by the user. 
+                                        "text": f"""You are a {responder_role} and are given insturctions by the user. 
                                         Answer the insturctions with under 10 words and ONLY in the format:
                                             response:
                                             reasoning:
-                                            YOU MAY NOT ANSWER WITHOUT USING THE FORMAT UNDER 10 WORDS."""
+                                            YOU MAY NOT ANSWER WITHOUT USING THE FORMAT AND UNDER 10 WORDS."""
                                     },  
                                     {
-                                        "type": "text", "text": """
-                                        You are playing the ultimatum game against 'gemma'.
+                                        "type": "text", "text": f"""
+                                        You are playing the ultimatum game against {proposer_role}.
                                         In stage 1 of the ultimatum game,
-                                        the first player (Proposer) proposes a specific split of a fixed amount of money of $100,
-                                        to the second player.
-                                        In stage 2 the second player (Responder) can either accept the proposed split or reject it.
+                                        the first player (Proposer) proposes a specific split of a fixed
+                                        amount of money of $100 to the second player.
+                                        In stage 2 the second player (Responder) can either accept 
+                                        the proposed split or reject it.
                                         If they accept the proposal, 
                                         the fixed amount is divided according to the first mover's proposal.
                                         If they choose to reject the proposal, the players do not receive anything."""},
                                     {
                                         "type": "text", "text": f"""You are the responder.
                                         Choose whether to 'accept' or 'reject' the proposal.
-                                        The proposal is {99} for the Proposer and {1} for you.
+                                        The proposal is {proposer_sum} for the Proposer and {responder_sum} for you.
                                         You may use information from previous games if any is available."""
                                     },
                                     {
-                                        "type": "text", "text": f"information from previous games: Gemma: {history}"
+                                        "type": "text", "text": f"information from previous games: {history}"
                                     }
                                 ]
-                            )
-        return proposal.split()[1], proposal.split()[-1]
+                             )
+        if responder == 'gemini':
+            response = self.gemini.invoke([message])
+        else:
+            response = self.mistral.invoke([message])
+        if isinstance(response, str):
+            response = response.lower()
+        else:
+            response = response.content.lower()
+        return response.split()[1], response.split('reasoning: ')[-1]
 
     @staticmethod
     def assign_roles(mode, player):
-        assert mode in np.arange(8)+1, /
+        assert mode in np.arange(8) + 1, \
         """
         mode can be set to 1-8:
             1: LLM (gemini) vs LLM (mistral)
--- models.py
+++ models.py
@@ -1,13 +1,10 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_huggingface.llms import HuggingFacePipeline
 import torch
-import gc
 import numpy as np
 np.random.seed(0)
 torch.manual_seed(0)
-
-def clear_gpu_memory():
-    torch.cuda.empty_cache()
-    gc.collect()
 
 
 def initialize_hugging_face_models(model_name: str, hugging_face_token: str):
@@ -15,82 +12,26 @@ def initialize_hugging_face_models(model_name: str, hugging_face_token: str):
     hugging_face_model = AutoModelForCausalLM.from_pretrained(model_name, token=hugging_face_token)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     hugging_face_model.to(device)
-    return hugging_face_model, tokenizer
+    pipe = pipeline("text-generation",
+                    model=hugging_face_model,
+                    tokenizer=tokenizer,
+                    max_new_tokens=20,
+                    temperature=0.9,
+                    do_sample=True,
+                    num_return_sequences=1)
+    hfp = HuggingFacePipeline(pipeline=pipe)
+    return hfp
 
 
-def initialize_llama3(hugging_face_token: str):
-    model_name = "meta-llama/Meta-Llama-3-8B-Instruct"
+def initialize_mistral(hugging_face_token: str):
+    model_name = "mistralai/Mistral-7B-Instruct-v0.3"
     return initialize_hugging_face_models(model_name, hugging_face_token)
 
 
-def initialize_gemma(hugging_face_token: str):
-    model_name = "google/gemma-7b-it"
-    return initialize_hugging_face_models(model_name, hugging_face_token)
-
-
-def get_response_google_gemma(user_prompt: str, model, hugging_face_model, tokenizer, verbose=1):
-    if verbose:
-        print(model + ': ', user_prompt, end=' ')
-
-    # Input text
-    input_text = """<start_of_turn>user
-    You are a helpful assistant. Answer the question without asking for additional information. 
-    User's question: {BODY}<end_of_turn>
-    <start_of_turn>model
-    """.format(BODY=user_prompt)
-
-    # Tokenize input text
-    input_ids = tokenizer.encode(input_text, return_tensors="pt").to(device)
-
-    # Generate text
-    output = hugging_face_model.generate(input_ids, temperature=0.5, do_sample=True,
-                                         max_new_tokens=1000, num_return_sequences=1)
-
-    # Decode generated text
-    generated_text = tokenizer.decode(output[0], skip_special_tokens=True).split('model\n')[1].strip()
-
-    # Clear CUDA memory
-    clear_gpu_memory()
-
-    if verbose==1:
-        print('Done.')
-    return generated_text
-
-
-def get_response_meta_llama(user_prompt: str, model='llama-2', verbose=1, hugging_face_model=None, tokenizer=None):
-    if verbose:
-        print(model + ': ', user_prompt, end=' ')
-        messages = [
-            {"role": "system", "content": """You are a helpful assistant. 
-            Answer the question without asking for additional information."""},
-            {"role": "user", "content": user_prompt},
-        ]
-        input_ids = tokenizer.apply_chat_template(messages,
-                                                    add_generation_prompt=True,
-                                                    return_tensors="pt"
-                                                ).to(device)
-
-        terminators = [
-            tokenizer.eos_token_id,
-            tokenizer.convert_tokens_to_ids("<|eot_id|>")
-        ]
-
-        output = hugging_face_model.generate(
-            input_ids,
-            max_new_tokens=1000,
-            eos_token_id=terminators,
-            do_sample=True,
-            temperature=0.5
-        )
-
-        # Decode generated text
-        response = output[0][input_ids.shape[-1]:]
-        generated_text = tokenizer.decode(response, skip_special_tokens=True)
-        print(generated_text)
-
-    # Clear CUDA memory
-    clear_gpu_memory()
-
-    if verbose == 1:
-        print('Done.')
-    return generated_text
+def initialize_gemini(google_api_key: str):
+    return ChatGoogleGenerativeAI(model="gemini-pro",
+                                  google_api_key=google_api_key, 
+                                  temperature=0.5, 
+                                  top_p=1,
+                                  top_k=1,
+                                  max_output_tokens=1000,)
