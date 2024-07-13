f6b2c1213c6eeee507c56826f237bc21268edec9
RZdataprojects <69355685+RZdataprojects@users.noreply.github.com>
2024-07-13 16:35:09 +0300

    final

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import experiments
+import experiment
+import argparse
 import torch
 import numpy as np
 np.random.seed(0)
@@ -6,8 +7,29 @@ torch.manual_seed(0)
 
 
 def main():
-    pass
+    parser = argparse.ArgumentParser(description='Run an LLM vs LLM experiment using the Ultimatum game and save the results.')
+    parser.add_argument('--google_api_key', type=str, required=True, help='Google API key')
+    parser.add_argument('--hugging_face_token', type=str, required=True, help='Hugging Face token')
+    parser.add_argument('--mode', type=int, required=True, help="""mode can be set to 1-8:
+            1: LLM (gemini) vs LLM (llama)
+            2: LLM (gemini) vs Human Male (llama)
+            3: "Human Male" (gemini) vs LLM (llama)
+            4: LLM (gemini) vs "Human Male" (llama)
+            5: "Human Female" (gemini) vs LLM (llama)
+            6: LLM (gemini) vs "Human Female" (llama)
+            7: "Human Male" (gemini) vs "Human Female" (llama)
+            8: "Human Female" (gemini) vs "Human Male" (llama)""")
+    parser.add_argument('--num_of_games', type=int, default=30, help='Number of games in a single experiment.')
+    parser.add_argument('--num_of_rounds', type=int, default=6, help='Number of rounds in a game.')
+    parser.add_argument('--save_path', type=str, default='./', help='Path to save the game data')
+    parser.add_argument('--verbose', type=int, default=1, help='Verbosity')
 
+    args = parser.parse_args()
+
+    e = experiment.Experiment(google_api_key=args.google_api_key, hugging_face_token=args.hugging_face_token, num_of_games=args.num_of_games, num_of_rounds=args.num_of_rounds, verbose=1)
+    game = e.play(mode=args.mode)
+    e.save_to_csv(game, save_path=args.save_path + "game_data.csv")
+    print(f'Game saved as "game_data.csv" in {args.save_path}')
 
 if __name__ == "__main__":
     main()
--- models.py
+++ models.py
@@ -1,37 +1,85 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_huggingface.llms import HuggingFacePipeline
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import google.generativeai as genai
 import torch
 import numpy as np
 np.random.seed(0)
 torch.manual_seed(0)
 
 
-def initialize_hugging_face_models(model_name: str, hugging_face_token: str):
-    tokenizer = AutoTokenizer.from_pretrained(model_name, token=hugging_face_token)
-    hugging_face_model = AutoModelForCausalLM.from_pretrained(model_name, token=hugging_face_token)
+def initialize_llama(hugging_face_token, device):
+    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct", token=hugging_face_token)
+    model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct", token=hugging_face_token)
+    model.to(device)
+    return model, tokenizer
+
+
+def initialize_mistral(hugging_face_token, device):
+    model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.3", device_map=0)
+    tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.3")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    hugging_face_model.to(device)
-    pipe = pipeline("text-generation",
-                    model=hugging_face_model,
-                    tokenizer=tokenizer,
-                    max_new_tokens=20,
-                    temperature=0.9,
-                    do_sample=True,
-                    num_return_sequences=1)
-    hfp = HuggingFacePipeline(pipeline=pipe)
-    return hfp
+    model.to(device)
+    return model, tokenizer
+    
+
+def initialize_gemini(google_api_key):
+    genai.configure(api_key=google_api_key)
+
+    # Set up the model
+    generation_config = {"temperature": 0.5, "top_p": 1, "top_k": 1, "max_output_tokens": 1000,}
+
+    safety_settings = [
+      {"category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_NONE"},
+      {"category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_NONE"},
+      {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_NONE"},
+      {"category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_NONE"},]
+
+    gemini_1_pro_client = genai.GenerativeModel(model_name="gemini-1.0-pro",
+                                  generation_config=generation_config,
+                                  safety_settings=safety_settings)
+    return gemini_1_pro_client
+    # return ChatGoogleGenerativeAI(model="gemini-pro",
+    #                               google_api_key=google_api_key, 
+    #                               temperature=0.99, 
+    #                               top_p=1,
+    #                               top_k=1,
+    #                               max_output_tokens=1000,)
 
 
-def initialize_mistral(hugging_face_token: str):
-    model_name = "mistralai/Mistral-7B-Instruct-v0.3"
-    return initialize_hugging_face_models(model_name, hugging_face_token)
+def call_llama(messages, model, tokenizer, device):
+    input_ids = tokenizer.apply_chat_template(messages,
+                                             add_generation_prompt=True,
+                                             return_tensors="pt").to(device)
+
+    terminators = [
+        tokenizer.eos_token_id,
+        tokenizer.convert_tokens_to_ids("<|eot_id|>")
+    ]
+
+    output = model.generate(
+        input_ids,
+        max_new_tokens=256,
+        eos_token_id=terminators,
+        do_sample=True,
+        temperature=0.99,
+        top_p=0.99
+    )
+    
+    # Decode generated text
+    response = output[0][input_ids.shape[-1]:]
+    generated_text = tokenizer.decode(response, skip_special_tokens=True)
+    return generated_text
 
 
-def initialize_gemini(google_api_key: str):
-    return ChatGoogleGenerativeAI(model="gemini-pro",
-                                  google_api_key=google_api_key, 
-                                  temperature=0.5, 
-                                  top_p=1,
-                                  top_k=1,
-                                  max_output_tokens=1000,)
+def call_mistral(messages, model, tokenizer, device):
+    inputs = tokenizer.apply_chat_template(messages, return_tensors="pt").to(device)
+    outputs = model.generate(inputs,
+                             max_new_tokens = 256,
+                             do_sample = True,
+                             temperature = 0.99,
+                             top_p = 0.99)
+    generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True).split("[/INST]")[1].strip()
+    return generated_text
